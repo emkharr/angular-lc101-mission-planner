742448ec15bef8854132efa99f88122c7c29ced9
Emily Harrison <ekharrison.art@gmail.com>
2022-03-22 21:16:04 -0600

    starting on bonus missions

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -8,14 +8,42 @@ import { Component, OnInit } from '@angular/core';
 export class CrewComponent implements OnInit {
 
   crew: object[] = [
-    {name: "Eileen Collins", firstMission: false},
-    {name: "Mae Jemison", firstMission: false},
-    {name: "Ellen Ochoa", firstMission: true}
+    { name: "Eileen Collins", firstMission: false },
+    { name: "Mae Jemison", firstMission: false },
+    { name: "Ellen Ochoa", firstMission: true }
   ];
-
+  memberBeingEdited: object = null;
+  inCrew:boolean = false;
+  
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(memberName: string, isFirst: boolean) {
+    for (let i=0; i < this.crew.length; i++) {
+        if(this.crew[i]['name'] === memberName) {
+            this.inCrew = true;
+        }
+    }
+    if (!this.inCrew) {
+        this.crew.push({name: memberName, firstMission: isFirst});
+    }
+    this.inCrew = false;
+  }
+
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
+    }
 }
